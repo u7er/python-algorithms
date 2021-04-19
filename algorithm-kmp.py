1bd4cfbd4952e23340853e93ec8be94b599065d2
Pavel Manakov <owfqw4a@gmail.com>
2021-04-19 12:07:49 +0300

    Обновление алгоритма

--- algorithm-kmp.py
+++ algorithm-kmp.py
@@ -1,8 +1,14 @@
-t = "лилила"
+t = "другой"
 
 p = [0]*len(t)
 j = 0
 i = 1
+
+text = ''
+import os
+HERE = str(os.path.dirname(os.path.abspath(__file__)))
+with open(HERE + '\\text.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
 
 while i < len(t):
     if t[j] == t[i]:
@@ -11,25 +17,26 @@ while i < len(t):
         j += 1
     else:
         if j == 0:
-            p[i] = 0;
+            p[i] = 0
             i += 1
         else:
             j = p[j-1]
 
 print(p)
 
-a = "лилилось лилилась"
+a = text
 m = len(t)
 n = len(a)
 
 i = 0
 j = 0
+isFound = False
 while i < n:
     if a[i] == t[j]:
         i += 1
         j += 1
         if j == m:
-            print("образ найден")
+            isFound = True
             break
     else:
         if j > 0:
@@ -37,5 +44,7 @@ while i < n:
         else:
             i += 1
 
-if i == n:
-    print("образ не найден")
+if i == n and not isFound:
+    print(f"образ не найден {j} {i}")
+
+print(f"образ найден на индексе {i}: {a[i-m-50:i-m]}{a[i-m:i].upper()}{a[i:i+50]}")
